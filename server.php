e8c2254402720c4ed9e51bb2dc41b0b957a01a5f
Jaagup Aland <jaagala@users.noreply.github.com>
2019-04-02 17:52:02 +0300

    Esialgsed failid

---
+++ server.php
@@ -0,0 +1,17 @@
+<?php
+  if(isset($_POST["save"]) && !empty($_POST["save"])){
+    saveToFile($_POST["save"]);
+  }
+
+  function saveToFile($stringToSave){
+    $object = new StdClass();
+    $object->date_modified = time();
+    $object->content = $stringToSave;
+    $jsonString = json_encode($object);
+    if(file_put_contents("database.txt", $jsonString)){
+      echo "success";
+    }
+  }
+
+
+ ?>
